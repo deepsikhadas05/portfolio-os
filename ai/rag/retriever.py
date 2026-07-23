22b074719dbc281f2b65e64324b38b19739dbc07
deepsikha05 <deepsikha1104@gmail.com>
2026-07-23 23:38:34 +0530

    modified the requirements.txt

--- ai/rag/retriever.py
+++ ai/rag/retriever.py
@@ -1,13 +1,17 @@
-from rag.vectorstore import vectorstore
+from rag.vectorstore import get_vectorstore
 
-retriever = vectorstore.as_retriever(
-    search_type="mmr",
-    search_kwargs={
-        "k": 4,
-        "fetch_k": 10,
-    },
-)
+
+def get_retriever():
+    vectorstore = get_vectorstore()
+    return vectorstore.as_retriever(
+        search_type="mmr",
+        search_kwargs={
+            "k": 4,
+            "fetch_k": 10,
+        },
+    )
 
 
 def retrieve(query: str):
+    retriever = get_retriever()
     return retriever.invoke(query)
--- ai/rag/vectorstore.py
+++ ai/rag/vectorstore.py
@@ -4,14 +4,23 @@ from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 DB_DIR = BASE_DIR / "db"
 
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+_embeddings = None
+_vectorstore = None
 
-vectorstore = Chroma(
-    persist_directory=str(DB_DIR),
-    embedding_function=embeddings,
-)
+
+def get_vectorstore():
+    global _embeddings, _vectorstore
+
+    if _vectorstore is None:
+        _embeddings = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
+
+        _vectorstore = Chroma(
+            persist_directory=str(DB_DIR),
+            embedding_function=_embeddings,
+        )
+
+    return _vectorstore
